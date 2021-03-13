2e06eb608d4b1907697047b9faeac44dfbf72166
abduhbatubara <abduhbatubara@student.telkomuniversity.ac.id>
2021-03-13 14:29:32 +0700

    Menampilkan data dengan adapter

---
+++ app/src/main/java/org/d3if3067/recycleview/Hewan.kt
@@ -0,0 +1,6 @@
+package org.d3if3067.recycleview
+
+data class Hewan(
+        val nama: String,
+        val namaLatin: String
+)
--- app/src/main/java/org/d3if3067/recycleview/MainActivity.kt
+++ app/src/main/java/org/d3if3067/recycleview/MainActivity.kt
@@ -2,10 +2,38 @@ package org.d3if3067.recycleview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import org.d3if3067.recycleview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d("MainActivity","Jumlah data: " + getData().size)
+        with(binding.recyclerView){
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+            adapter =  MainAdapter(getData())
+            setHasFixedSize(true)
+        }
+    }
+    private fun getData(): List<Hewan>{
+        return listOf(
+            Hewan("Angsa","Cygnus olor"),
+            Hewan("Ayam","Gallus gallus"),
+            Hewan("Bebek","Cairina moschata"),
+            Hewan("Domba","Ovis ammon"),
+            Hewan("Kalkun","Meleagris gallopavo"),
+            Hewan("Kambing","Capricornis sumatrensis"),
+            Hewan("Kelinci", "Oryctolagus cuniculus"),
+            Hewan("Kerbau","Bubalus bubalis"),
+            Hewan("Kuda", "Equus caballus"),
+            Hewan("Sapi","Bos taurus"),
+
+        )
     }
 }
