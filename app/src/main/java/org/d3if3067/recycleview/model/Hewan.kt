cdec7b99eb2f531bb089c49b456923be6b27ff57
Muhammad_Abduh_Husaini_Batubara <abduhbatubara@student.telkomuniversity.ac.id>
2021-05-08 14:28:09 +0700

    3.4.Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if3067/recycleview/model/Hewan.kt
+++ app/src/main/java/org/d3if3067/recycleview/model/Hewan.kt
@@ -1,8 +1,10 @@
 package org.d3if3067.recycleview.model
 
+import org.d3if3067.recycleview.R
+
 data class Hewan(
         val nama: String,
         val namaLatin: String,
-        val imageResId: Int,
+        val imageResId: Int = R.drawable.angsa,
         val jenisHewan: String
 )
